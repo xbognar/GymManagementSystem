1a7b4420997998cc1a053f3cb43ba43cfb07936f
Mate <mattheus.bognar@gmail.com>
2025-01-21 13:59:30 +0100

    Marking the DbSet Properties to virtual - allowing Moq to ovveride them.

--- src/GymDBAccess/DataAccess/ApplicationDbContext.cs
+++ src/GymDBAccess/DataAccess/ApplicationDbContext.cs
@@ -1,26 +1,20 @@
 ﻿using GymDBAccess.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GymDBAccess.DataAccess
 {
 	public class ApplicationDbContext : DbContext
 	{
-
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
 			: base(options)
 		{
 		}
-		
-		public DbSet<Member> Members { get; set; }
-		
-		public DbSet<Membership> Memberships { get; set; }
-		
-		public DbSet<Chip> Chips { get; set; }
 
+		// Make DbSet<T> properties virtual for Moq
+		public virtual DbSet<Member> Members { get; set; }
+		
+		public virtual DbSet<Membership> Memberships { get; set; }
+		
+		public virtual DbSet<Chip> Chips { get; set; }
 	}
 }
